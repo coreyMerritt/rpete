4d1c7d492ccc17ff8696b58e115156e8ecb262e5
Corey <coreyleemerritt@gmail.com>
2024-03-09 11:28:31 -0500

    Fixed an issue where the delay in two parts of the code didn't match. Added comments and have begun debugging.

--- rPete.py
+++ rPete.py
@@ -4,12 +4,35 @@ import time
 timestamps = []
 error_margin = []
 iterations = 10
+pause = 0.3
 
-for i in range(iterations):
-    timestamps.append(pendulum.now())
-    time.sleep(0.1)
-    timestamps.append(pendulum.now())
-    error_margin.append(abs(500000 - abs(timestamps[(i*2)+1].microsecond - timestamps[i*2].microsecond)))
+'''
+for i in range(iterations):                                                                                             
+    #Stores 2 timestamps that are a given interval apart.
+    timestamps.append(pendulum.now().microsecond)               
+    time.sleep(pause)
+    timestamps.append(pendulum.now().microsecond)                                                                                
+    
+    #Debugging
+    print(str(abs((pause * 1000000) - abs(timestamps[(i*2)+1] - timestamps[i*2]))) + " ~800")
+    
+    #(ts2 - ts1) - expected interval = error margin 
+    error_margin.append(abs((pause * 1000000) - abs(timestamps[(i*2)+1].microsecond - timestamps[i*2].microsecond)))
+   
+    #Let the user know how far along we are. 
     print("Pass " + str(i))
 
+#Final result.
 print("Average error margin was " + str((sum(error_margin) / iterations) / 1000) + "ms.")
+'''
+
+#Debugging
+#Lets just check the math one step at a time. For starters I should move to storing only the microsecond values into the timestamps list, storing the
+#entire timestamp was just silly and can only hurt our error margin. These changes are also reflected above now.
+timestamps.append(pendulum.now().microsecond)
+time.sleep(pause)
+timestamps.append(pendulum.now().microsecond)
+
+print(timestamps[0])
+print(timestamps[1])
+print(timestamps[1]- timestamps[0])
