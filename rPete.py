8ab408e54a72d52f8a47b10c8f61b273d6687fe0
Corey <coreyleemerritt@gmail.com>
2024-03-09 12:36:07 -0500

    Migrated our new algorithm over and cleaned things up a bit.

--- rPete.py
+++ rPete.py
@@ -5,41 +5,26 @@ timestamps = []
 error_margin = []
 iterations = 10
 pause = 0.3
+delay = 0
 
-'''
+
 for i in range(iterations):                                                                                             
     #Stores 2 timestamps that are a given interval apart.
     timestamps.append(pendulum.now().microsecond)               
     time.sleep(pause)
     timestamps.append(pendulum.now().microsecond)                                                                                
-    
-    #Debugging
-    print(str(abs((pause * 1000000) - abs(timestamps[(i*2)+1] - timestamps[i*2]))) + " ~800")
-    
-    #(ts2 - ts1) - expected interval = error margin 
-    error_margin.append(abs((pause * 1000000) - abs(timestamps[(i*2)+1].microsecond - timestamps[i*2].microsecond)))
    
+
+    #Normalize the delay and handles negative values; Calculates the error margin.
+    if (timestamps[i*2+1] - timestamps[i*2] > 0):
+        delay = timestamps[i*2+1] - timestamps[i*2]
+    else:
+        delay = 1000000 + (timestamps[i*2+1] - timestamps[i*2])     #1000000 is the constant for microseconds in 1s.
+    error_margin.append(int(delay - (1000000 * pause)))
+   
+
     #Let the user know how far along we are. 
     print("Pass " + str(i))
-
+ 
 #Final result.
 print("Average error margin was " + str((sum(error_margin) / iterations) / 1000) + "ms.")
-'''
-
-#Debugging
-timestamps.append(pendulum.now().microsecond)
-time.sleep(pause)
-timestamps.append(pendulum.now().microsecond)
-
-print(timestamps[0])
-print(timestamps[1])
-
-delay = 0
-
-if (timestamps[1] - timestamps[0] > 0):
-    delay = timestamps[1] - timestamps[0]
-else: 
-    delay = 1000000 + (timestamps[1] - timestamps[0])    #1000000 is the constant for microseconds in 1s.
-
-#This is subtract the expected delay from our actual delay, leaving our proper error margin.
-print(int(delay - (1000000 * pause)))
