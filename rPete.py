c013adbfff71980d2810c113ad1631862a57f55e
Corey <coreyleemerritt@gmail.com>
2024-03-09 14:19:19 -0500

    Edited the user input prompt to avoid user error.

--- rPete.py
+++ rPete.py
@@ -3,9 +3,11 @@ import time
 
 timestamps = []
 error_margin = []
-iterations = 10
-pause = 0.3
 delay = 0
+
+#Gets user input
+iterations = int(input("How many iterations would you like to run?\n"))
+pause = float(input("How long would you like the pause between tests to be? (In Seconds) 0.01 - 0.99\n"))
 
 
 #Converts Seconds to Microseconds
@@ -22,8 +24,7 @@ for i in range(iterations):
     #Stores 2 timestamps that are a given interval apart
     timestamps.append(pendulum.now().microsecond)               
     time.sleep(pause)
-    timestamps.append(pendulum.now().microsecond)                                                                                
-   
+    timestamps.append(pendulum.now().microsecond)                                                                                 
 
     #Normalize the delay and handles negative values
     if (timestamps[i*2+1] - timestamps[i*2] > 0):
@@ -33,7 +34,6 @@ for i in range(iterations):
     #Calculates the error margin
     error_margin.append(int(delay - (secToMicro(pause))))
    
-
     #Let the user know how far along we are. 
     print("Pass " + str(i))
  
