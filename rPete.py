d3b52799c66ff44791e23767228e7318f05ed54e
Corey <coreyleemerritt@gmail.com>
2024-03-09 10:36:20 -0500

    The automation is in place and works well now, but the math is off somewhere. Going to use some debugging methods to check my formula.

--- rPete.py
+++ rPete.py
@@ -1,12 +1,15 @@
 import pendulum
 import time
 
-timestamps = [pendulum.now()]
-print(timestamps[0].microsecond)
+timestamps = []
+error_margin = []
+iterations = 10
 
-time.sleep(0.5)
+for i in range(iterations):
+    timestamps.append(pendulum.now())
+    time.sleep(0.1)
+    timestamps.append(pendulum.now())
+    error_margin.append(abs(500000 - abs(timestamps[(i*2)+1].microsecond - timestamps[i*2].microsecond)))
+    print("Pass " + str(i))
 
-timestamps.append(pendulum.now())
-print(timestamps[1].microsecond)
-
-print(abs(500000 - abs(timestamps[1].microsecond - timestamps[0].microsecond)))
+print("Average error margin was " + str((sum(error_margin) / iterations) / 1000) + "ms.")
