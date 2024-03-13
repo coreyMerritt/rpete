391d611f5db133e645104362f32e5f408f64f8c3
coreyMerritt <coreyleemerritt@gmail.com>
2024-03-13 19:11:46 -0400

    Cleaned up for readability.

--- error_margin.py
+++ error_margin.py
@@ -1,18 +1,7 @@
 #This program tests the accuracy of the pendulum and time libraries and concludes that the error margin
 #is approximately 0.3ms to 1.3ms and seems highly dependant on the delay given to the sleep() function
 #inside the time library. This margin seems plenty small enough to use these libraries for rPete.
-
-import pendulum
-import time
-
-timestamps = []
-error_margin = []
-delay = 0
-
-#Gets user input
-iterations = int(input("How many iterations would you like to run?\nEnter an integer value from: 1 - 999\n"))
-pause = float(input("How long would you like the pause between tests to be? (In Seconds)\nEnter a decimal value from: 0.01 - 0.99\n"))
-
+import pendulum, time
 
 #Converts Seconds to Microseconds
 def secToMicro(sec):
@@ -22,6 +11,15 @@ def secToMicro(sec):
 #Converts Microseconds to Miliseconds
 def microToMili(micro):
     return round(micro / 1000, 2)
+
+
+timestamps = []
+error_margin = []
+delay = 0
+
+#Gets user input
+iterations = int(input("How many iterations would you like to run?\nEnter an integer value from: 1 - 999\n"))
+pause = float(input("How long would you like the pause between tests to be? (In Seconds)\nEnter a decimal value from: 0.01 - 0.99\n"))
 
 
 for i in range(iterations):                                                                                             
