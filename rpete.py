ddecb4ef1bfc167fcff4376f7099cfff6d1bb96e
coreyMerritt <156148024+coreyMerritt@users.noreply.github.com>
2024-04-05 19:48:36 -0400

    Fixed some spacing issues.

--- rpete.py
+++ rpete.py
@@ -1,20 +1,20 @@
 import os, sys, pendulum, curses
 
 
-#Converts Seconds to Microseconds.
+# Converts Seconds to Microseconds.
 def secToMili(sec):
     return sec * 1000
 
-#Converts Seconds to Microseconds.
+# Converts Seconds to Microseconds.
 def secToMicro(sec):
     return sec * 1000000
 
-#Converts Microseconds to Miliseconds.
+# Converts Microseconds to Miliseconds.
 def microToMili(micro):
     return round(micro / 1000, 2)
 
 
-#Demands that user runs with sudo, the Curses library requires root privileges.
+# Demands that user runs with sudo, the Curses library requires root privileges.
 if os.name == 'posix':   
    if os.geteuid() != 0:
         print("This script requires superuser privileges. Please run it with sudo.")
@@ -24,14 +24,14 @@ if os.name == 'posix':
 def main(display):
     curses.echo()
     
-    #Gets user input regarding test scope.
+    # Gets user input regarding test scope.
     display.addstr("How many keystrokes would you like to test? (A higher # will take longer, but will be more accurate)\n")
     display.addstr("\nEnter an integer value from 1-1000:\n")
     display.refresh()
     key_count = int(display.getstr().decode())
     
 
-    #Prompts the user to hold a key to collect repeat rate/delay data.
+    # Prompts the user to hold a key to collect repeat rate/delay data.
     display.clear()
     display.addstr("Press and hold the Spacebar key when ready. Do not release Spacebar until told to.\n")
     display.refresh()
@@ -41,7 +41,7 @@ def main(display):
     while True:
         timestamps = []
     
-        #Collects timestamps correlating to delay between keypresses while holding a key.
+        # Collects timestamps correlating to delay between keypresses while holding a key.
         first_pass = True
         user_error = False
         last_ts = pendulum.now()
@@ -62,7 +62,7 @@ def main(display):
     repeat_rate = []
     repeat_delay = 0
 
-    #Appends the repeat delay/rate after handling negative values.
+    # Appends the repeat delay/rate after handling negative values.
     for i in range(1, len(timestamps)):
         if i == 1:
             if (timestamps[i] - timestamps[i-1] > 0):
@@ -78,13 +78,13 @@ def main(display):
 
     rr_total = 0
 
-    #Calculates the average repeat rate from all values in the list.
+    # Calculates the average repeat rate from all values in the list.
     for x in repeat_rate:
         rr_total += x
     rr_avg = rr_total / len(repeat_rate)
 
 
-    #Displays results for user.
+    # Displays results for user.
     display.clear()
     display.addstr("Your repeat delay is: " + str(int(microToMili(repeat_delay))) + "ms")
     display.addstr("\nYour repeat rate is: " + str(int(microToMili(rr_avg))) + "ms\n")
@@ -94,7 +94,7 @@ def main(display):
 
     keystroke = 0
     
-    #Persists the program until user presses Enter.
+    # Persists the program until user presses Enter.
     while keystroke != 10:
         keystroke = display.getch()
 
